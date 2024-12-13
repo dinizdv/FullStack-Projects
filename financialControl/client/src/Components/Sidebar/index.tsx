e2e99bcd1f7eea37a23fae7f247e65a6a22f420a
dinizdv <dinizdev3@gmail.com>
2024-12-13 12:26:39 -0300

    feat(Sidebar, Summary): initials desktop interfaces.

--- financialControl/client/src/Components/Sidebar/index.tsx
+++ financialControl/client/src/Components/Sidebar/index.tsx
@@ -1,7 +1,32 @@
+import '../../styles/sidebar.css'
+import { IoHomeOutline } from "react-icons/io5";
+import { GoGoal } from "react-icons/go";
+import { TbReportSearch } from "react-icons/tb";
+import { VscGraphLine } from "react-icons/vsc";
+import { CgOptions } from "react-icons/cg";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoMoonOutline } from "react-icons/io5";
+import { FiSun } from "react-icons/fi";
+
+
 const Sidebar = () => {
     return(
-        <div>
-            <h1>sidebar</h1>
+        <div className='sidebar'>
+            
+            <div className="logo-sidebar">
+                    <label>Financial Control</label>
+            </div>
+            <nav className='nav-sidebar'>
+                <ul className="ul-sidebar">
+                    <li className="li-sidebar"><IoHomeOutline className='icon-sidebar' />summary</li>
+                    <li className="li-sidebar"><GoGoal className='icon-sidebar'/>budget</li>
+                    <li className="li-sidebar"><TbReportSearch className='icon-sidebar'/>report</li>
+                    <li className="li-sidebar"><VscGraphLine className='icon-sidebar'/>graphs</li>
+                    <li className="li-sidebar"><CgOptions className='icon-sidebar'/>categories</li>
+                    <li className="li-sidebar"><IoSettingsOutline className='icon-sidebar'/>settings</li>
+                    <li className="li-sidebar"><IoMoonOutline className='icon-sidebar'/>dark mode</li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- financialControl/client/src/pages/Summary/index.tsx
+++ financialControl/client/src/pages/Summary/index.tsx
@@ -1,6 +1,55 @@
+import '../../styles/summary.css'
+import Sidebar from '../../Components/Sidebar'
+import { useState, useEffect } from 'react'
+
 const Summary = () => {
+    const [date, setDate] = useState<String | undefined>(undefined)
+
+    useEffect(() => {
+        const currentDate = new Date().toLocaleDateString()
+        setDate(currentDate)
+    }, [])
+
     return (
-        <h1>Summary</h1>
+            <>
+                <Sidebar />
+                    <div className="container-summary">
+
+                        <div className="container-style-summary">
+                            <section className="section-summary current-month">
+                                <h2>balance</h2>
+                                <label htmlFor="">$439.9K</label>
+                            </section>
+                        
+                            <section className="section-summary incomes">
+                                <h2>incomes</h2>
+                                <label htmlFor="">$439.9K</label>
+                            </section>
+
+                            <section className="section-summary expenses">
+                                <h2>expenses</h2>
+                                <label htmlFor="">$439.9K</label>
+                            </section>
+
+                        <section className="section-summary incomes-and-expenses">
+                                <h2>incomes and expenses (last 6 months)</h2>
+                                <label htmlFor="">$439.9K</label>
+                            </section>
+                        <section className="section-summary expenses-by-categories">
+                                <h2>expenses by categories</h2>
+                                <label htmlFor="">$439.9K</label>
+                            </section>
+                            <section className="section-summary incomes-by-categories">
+                                <h2>incomes by categories</h2>
+                                <label htmlFor="">$439.9K</label>
+                            </section>
+                            </div>
+
+        <div className="container-date">
+            <label>Today is {date}</label>
+        </div>
+                    </div>
+            </>
     )
 }
 
